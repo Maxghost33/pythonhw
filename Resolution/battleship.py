27716c75a883e2da7a802aa2410b34a8ae0a3a9a
MaxGhost33 <maxim.jovanik@gmail.com>
2018-11-04 04:29:01 -0500

    Last versions basic tasks

--- Resolution/battleship.py
+++ Resolution/battleship.py
@@ -2,17 +2,18 @@ import random
 
 rang = int(raw_input("Choose size of battlefield: "))
 
-
-
 def print_board(board):
     for row in board:
         print " ".join(row)
 
+def graph_board(board, row, col, value):
+    board[row - 1][col - 1] = value
+
 def random_row(board):
-    return random.randint(0, len(board) - 1)
+    return random.randint(1, len(board))
 
 def random_col(board):
-    return random.randint(0, len(board[0]) - 1)
+    return random.randint(1, len(board[0]))
 
 
 def play(board):
@@ -23,35 +24,40 @@ def play(board):
     ship_col = random_col(board)
     print ship_row * 2
     print ship_col * 2
-    turn = 0
-    for turn in range(5):
-        turn = turn + 1
+    turn = 1
+    for turn in range(rang+1):
+        turn = turn
         print "Attempt#", turn 
         guess_row = int(raw_input("Guess Row:"))
         guess_col = int(raw_input("Guess Col:"))
-
+        
         if guess_row == ship_row and guess_col == ship_col:
+            graph_board(board, guess_row, guess_col, "X")
+            print_board(board)
             print "Congratulations! You sunk my battleship!"
             break
+        if (guess_row <= 0 or guess_row > rang) or (guess_col <= 0 or guess_col > rang):
+            print "Oops, that's not even in the ocean. Try again!"
         else:
-            if (guess_row < 0 or guess_row > rang) or (guess_col < 0 or guess_col > rang):
-                print "Oops, that's not even in the ocean."
-            #elif(board[guess_row][guess_col] == "X"):
-            #    print "You guessed that one already."
+            if (board[guess_row - 1][guess_col - 1] == "o"):
+                print_board(board)
+                print "You guessed that one already. Try again!"
             else:
                 if turn == rang:
-                    print "Game Over"
+                    graph_board(board, guess_row, guess_col, "o")
+                    print_board(board)
+                    print "Game Over. You are loser!"
                     break
                 else:
-                    print "You missed my battleship!"
-                    #board[guess_row][guess_col] = "X"
+                    graph_board(board, guess_row, guess_col, "o")
                     print_board(board)
-
+                    print "You missed my battleship! Try again!"
 
 def main():
     board = []
     for x in range(rang):
-        board.append(["O"] * rang)
+        board.append(["*"] * rang)
+    #print (board)
     play(board)
 
 
